b84ae8b77ecf8b41279e0e72857819ae8baee49b
SamiranSamanta2023 <138391313+SamiranSamanta2023@users.noreply.github.com>
2024-11-06 19:18:54 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,1 +1,20 @@
+const sql = require('mssql');
+
+const config = {
+  user: 'ServerAdmin',
+  password: 'Samanta123@',
+  server: 'samiran.database.windows.net',  // replace with your server
+  database: 'TestDB',
+  options: {
+    encrypt: true,
+    enableArithAbort: true
+  }
+};
+
+sql.connect(config, (err) => {
+  if (err) console.log('Database connection failed: ', err);
+  else console.log('Database connected successfully');
+});
+
+module.exports = sql;
 
